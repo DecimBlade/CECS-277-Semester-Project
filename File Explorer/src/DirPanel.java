130e26ae5e2e078a2c723d6bbebea115be254987
Steven U <starrysteven@yahoo.com>
2021-05-03 19:12:52 -0700

    Update DirPanel.java

--- File Explorer/src/DirPanel.java
+++ File Explorer/src/DirPanel.java
@@ -10,15 +10,22 @@ public class DirPanel extends JPanel {
 
     public DirPanel(){
         scrollPane.setViewportView(dirTree);
+        dirTree.setShowsRootHandles(true);
+        dirTree.setEditable(true);
+        dirTree.addTreeSelectionListener(new TreeListenerBro());
+        GroupLayout layout = new GroupLayout(this);
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE));
+        layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE));
         add(scrollPane);
+        treeBuilder(dir);
     }
 
     public void treeBuilder(String dir){
         File file = new File(dir);
-        //dirTree.addTreeSelectionListener(new MyTreeListener());
+        dirTree.addTreeSelectionListener(new MyTreeListener());
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(dir);
         DefaultTreeModel treeModel = new DefaultTreeModel(root);
-        //fileCreator(root, file);
+        fileCreator(root, file);
         dirTree.setModel(treeModel);
     }
 }
