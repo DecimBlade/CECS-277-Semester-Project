bbc1b6bcf974e282f3897f7faf6612b720650b41
Steven U <starrysteven@yahoo.com>
2021-05-03 17:18:37 -0700

    Update DirPanel.java

--- File Explorer/src/DirPanel.java
+++ File Explorer/src/DirPanel.java
@@ -1,4 +1,7 @@
+import java.io.File;
+
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 
 public class DirPanel extends JPanel {
@@ -8,5 +11,14 @@ public class DirPanel extends JPanel {
     public DirPanel(){
         scrollPane.setViewportView(dirTree);
         add(scrollPane);
+    }
+
+    public void treeBuilder(String dir){
+        File file = new File(dir);
+        dirTree.addTreeSelectionListener(new MyTreeListener());
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(dir);
+        DefaultTreeModel treeModel = new DefaultTreeModel(root);
+        fileCreator(root, file);
+        dirTree.setModel(treeModel);
     }
 }
